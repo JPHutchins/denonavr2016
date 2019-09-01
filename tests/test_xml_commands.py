3a80d5b73f54bb8ce2a6073c34d943d9b03b314e
J.P. Hutchins <jphutchins@gmail.com>
2019-09-01 12:45:49 -0700

    Let logger to console during errror tests.

--- tests/test_xml_commands.py
+++ tests/test_xml_commands.py
@@ -4,8 +4,13 @@
 Test make_xml_command.py
 """
 
+import sys
 import unittest
+import logging
 from denonavr2016.helpers import make_xml_command, XmlCommand1, XmlCommand3
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
 
 SET_DYNAMIC_VOL = XmlCommand3(
     "Dynamic Volume", "SetAudyssey",
@@ -29,6 +34,9 @@ SET_DIALOG_LEVEL = XmlCommand1(
 )
 
 class TestXmlCommands(unittest.TestCase):
+
+#------------------------------------------------------------------------------
+#   DICTIONARY LOOKUP
 
     def test_dict(self):
         """
@@ -54,14 +62,39 @@ b'<?xml version=\'1.0\' encoding=\'utf8\'?>\n\
 <tx><cmd id="3"><name>SetAudyssey</name><list>\
 <param name="dynamicvol">3</param></list></cmd></tx>')
 
-    def test_int_instead_of_string(self):
+#------------------------------------------------------------------------------
+#   ERROR HANDLING
+
+#   Let the appropriate log messages through to console
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(stream_handler)
+    logger.removeHandler(stream_handler)
+
+    def test_error_handling(self):
         """
-        Test the Try/Except that handles entering an int instead of string.
+        Test the Try/Except.
         """
+        #bad keys
+        self.assertEqual(make_xml_command(SET_DYNAMIC_VOL, "On"), None)
+        self.assertEqual(make_xml_command(SET_DIALOG_LEVEL, "13dB"), None)
+        #int instead of string
         self.assertEqual(make_xml_command(SET_DYNAMIC_VOL, 0),
 b'<?xml version=\'1.0\' encoding=\'utf8\'?>\n\
 <tx><cmd id="3"><name>SetAudyssey</name><list>\
 <param name="dynamicvol">0</param></list></cmd></tx>')
+        #value out of bounds too low
+        self.assertEqual(make_xml_command(SET_LFE, -11),
+b'<?xml version=\'1.0\' encoding=\'utf8\'?>\n\
+<tx><cmd id="3"><name>SetSurroundParameter</name><list>\
+<param name="lfe">-10</param></list></cmd></tx>')
+        #value out of bounds too high
+        self.assertEqual(make_xml_command(SET_LFE, 1),
+b'<?xml version=\'1.0\' encoding=\'utf8\'?>\n\
+<tx><cmd id="3"><name>SetSurroundParameter</name><list>\
+<param name="lfe">0</param></list></cmd></tx>')
+
+#------------------------------------------------------------------------------
+#   CLASS ATTRIBUTES MISSING
 
     def test_missing_values(self):
         """
@@ -71,6 +104,9 @@ b'<?xml version=\'1.0\' encoding=\'utf8\'?>\n\
 b'<?xml version=\'1.0\' encoding=\'utf8\'?>\n\
 <tx><cmd id="3"><name>SetSurroundParameter</name><list>\
 <param name="lfe">-5</param></list></cmd></tx>')
+
+#------------------------------------------------------------------------------
+#   CMD_ID=1 SYNTAX
 
     def test_cmd_id_1_commands(self):
         """
