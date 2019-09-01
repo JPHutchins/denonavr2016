dbbed4d1e6c29a3e2ec1431925292bf52c638adc
J.P. Hutchins <jphutchins@gmail.com>
2019-08-31 17:41:09 -0700

    Update syntax for copying commands from XML.

---
+++ denonavr2016/commands.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module contains instances of the XmlCommand class that are representations
+of the Denon AVR 2016 XML command structure.  Refer to ./XML_data_dump.txt for
+more information or to find out how to sniff commands on your own AVR.
+"""
+from .helpers import XmlCommand
+
+SET_DYNAMIC_VOL = XmlCommand(
+    "Dynamic Volume", "3", "SetAudyssey",
+    (0, 3), param="dynamicvol",
+    values=[
+        "Off",
+        "Light",
+        "Medium",
+        "Heavy"]
+)
+SET_LFE = XmlCommand(
+    "LFE Level", "3", "SetSurroundParameter",
+    (-10, 0), param="lfe")
